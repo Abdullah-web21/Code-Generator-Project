0cb582654f762022175ea02a47c3e111131e0973
Abdullah <web21@toppagerankers.com>
2025-10-23 02:52:48 +0500

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState,useCallback,useEffect,useRef} from 'react'
+import './App.css'
+import Cgenerator from './cgenerator'
+
+function App() {
+  
+
+  return (
+    <div>
+      <Cgenerator />
+    </div>
+     
+  )
+}
+
+export default App
