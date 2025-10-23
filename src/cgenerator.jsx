d6ec4c449efcea46fe94f137db006ae036c86ef9
Abdullah <web21@toppagerankers.com>
2025-10-23 22:07:38 +0500

    Initial commit

--- src/cgenerator.jsx
+++ src/cgenerator.jsx
@@ -1,97 +1,111 @@
-import { useState,useCallback,useEffect,useRef} from 'react'
-import './App.css'
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function Cgenerator() {
-  const [length,setlength] = useState(8)
-  const [numberAllowed,setnumberAllowed] = useState(true)
-  const [characterAllowed,setcharacterAllowed] = useState(true)
-  const [password,setpassword] = useState("")
-  const passwordRef = useRef(null)
+  const [length, setlength] = useState(8);
+  const [numberAllowed, setnumberAllowed] = useState(true);
+  const [characterAllowed, setcharacterAllowed] = useState(true);
+  const [password, setpassword] = useState("");
+  const passwordRef = useRef(null);
 
-  const generatePassword =useCallback(() => {
-    let pass =""
-    let str =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdedghijklmnopqrstuvwxyz"
-    if(numberAllowed) str+= "0123456789"
-    if(characterAllowed) str += "!@#$%^&*()_+}|{[]"
-    for (let i = 0; i <= length; i++) {
-      let char = Math.floor(Math.random() * str.length + 1)
-      pass += str.charAt(char)
-      
+  const generatePassword = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdedghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (characterAllowed) str += "!@#$%^&*()_+}|{[]";
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
     }
-    setpassword(pass)
-  },[length,numberAllowed,characterAllowed,setpassword])
-  useEffect( () => {
-    generatePassword()
-  },[length,numberAllowed,setpassword,characterAllowed])
+    setpassword(pass);
+  }, [length, numberAllowed, characterAllowed]);
+
+  // useEffect( () => {
+  //   generatePassword()
+  // },[length, numberAllowed, characterAllowed])
+
   const copyPasswordToClipboard = useCallback(() => {
     passwordRef.current?.select();
-
-    window.navigator.clipboard.writeText(password)
-
-  },[password])
- 
-
-
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
 
   return (
-     <div className="w-full h-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-16  bg-[#005f73]">
-      <h1 className='text-[#fca311] text-center text-3xl my-5'>PASSWORD GENERATOR</h1>
-     <div className="flex shadow rounded-lg overflow-hidden mb-6">
-        <input
-            type="text"
-            value={password}
-            className="outline-none bg-[#e5e5e5] w-full py-1 px-3 text-[#000000]"
-            placeholder="Password"
-            readOnly
-            ref={passwordRef}
-        />
-        <button
-        onClick={copyPasswordToClipboard}
-        className='outline-none bg-[#000000] text-[#fca311] px-3 py-0.5 shrink-0 text-1xl'
-        >copy</button>
-        
-    </div>
-    <div className='flex text-sm gap-x-2 mb-5'>
-      <div className='flex items-center gap-x-1'>
-        <input 
-        type="range"
-        min={8}
-        max={100}
-        value={length}
-         className='cursor-pointer'
-         onChange={(e) => setlength(e.target.value)}
-          />
-          <label className='text-[#fca311] '>Length : {length}</label>
-      </div>
-      <div className="flex items-center gap-x-1">
-      <input
-          type="checkbox"
-          className="cursor-pointer"
-          defaultChecked={numberAllowed}
-          id="numberInput"
-          onChange={() => {
-              setnumberAllowed((prev) => !prev);
-          }}
-      />
-      <label htmlFor="numberInput" className='text-[#fca311]'>Numbers</label>
-      </div>
-      <div className="flex items-center gap-x-1">
-          <input
-              type="checkbox"
-              className="cursor-pointer"
-              defaultChecked={characterAllowed}
-              id="characterInput"
-              onChange={() => {
-                  setcharacterAllowed((prev) => !prev )
-              }}
-          />
-          <label htmlFor="characterInput" className='text-[#fca311]'>Characters</label>
-      </div>
-    </div>
-</div>
+    <div className="w-full h-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-16  bg-[#005f73]">
+      <h1 className="text-[#fca311] text-center text-3xl my-5">
+        PASSWORD GENERATOR
+      </h1>
 
-  )
+      <div className="flex shadow rounded-lg overflow-hidden mb-6">
+        <input
+          type="text"
+          value={password}
+          className="outline-none bg-[#e5e5e5] w-full py-1 px-3 text-[#000000]"
+          placeholder="Password"
+          readOnly
+          ref={passwordRef}
+        />
+
+        <button
+          onClick={copyPasswordToClipboard}
+          className="outline-none bg-[#000000] text-[#fca311] px-3 py-0.5 shrink-0 text-1xl cursor-pointer"
+        >
+          copy
+        </button>
+      </div>
+
+      <div className="flex text-sm gap-x-2 mb-5">
+        <div className="flex items-center gap-x-1">
+          <input
+            type="range"
+            min={8}
+            max={30}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => setlength(e.target.value)}
+          />
+          <label className="text-[#fca311] ">Length : {length}</label>
+        </div>
+
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            className="cursor-pointer"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={() => {
+              setnumberAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="numberInput" className="text-[#fca311]">
+            Numbers
+          </label>
+        </div>
+
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            className="cursor-pointer"
+            defaultChecked={characterAllowed}
+            id="characterInput"
+            onChange={() => {
+              setcharacterAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="characterInput" className="text-[#fca311]">
+            Characters
+          </label>
+        </div>
+      </div>
+
+      <div className="outline-none bg-transparent text-[#fca311] px-4 py-2 shrink-0 text-1xl rounded-4xl>  w-full text-center">
+        <button
+          onClick={generatePassword}
+          className="outline-none bg-[#000000] text-[#fca311] px-4 py-2 shrink-0 text-1xl rounded-4xl cursor-pointer"
+        >
+          Generate Password
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Cgenerator
+export default Cgenerator;
